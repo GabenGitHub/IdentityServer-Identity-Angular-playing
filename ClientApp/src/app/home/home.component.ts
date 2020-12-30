430e49a000e744f2c8e5f89eeadd979a9951c9d5
Gabor Szabo <gabor.szabo86@hotmail.com>
2020-12-30 17:50:43 +0100

    Check user on client side

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  public isUserAuthenticated = false;
+
+  constructor(private _authService: AuthService) { }
+
+  ngOnInit(): void {
+    this._authService.loginChanged
+    .subscribe(res => {
+      this.isUserAuthenticated = res;
+    });
+  }
+
+  public getUser = () => this._authService.getUser();
 }
